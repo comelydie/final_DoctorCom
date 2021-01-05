26070475e37728dc3f6b41ef062f3e20448c4dfe
comelydie <comelydie@gmail.com>
2021-01-05 15:29:45 +0700

    main

---
+++ build/app/generated/source/buildConfig/release/th/ac/ssru/doctor_com/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package th.ac.ssru.doctor_com;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "th.ac.ssru.doctor_com";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
